393dc3f17b6cf9adcc4f7cae82d01fd1006edb62
cfletemier <chris.fletemier@inmar.com>
2019-06-04 23:04:28 -0400

    show all ten images in a fat bordered box

---
+++ dark_souls_level_calculator/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <ClassSelection/>
+  </div>
+</template>
+
+<script>
+import ClassSelection from './components/ClassSelection.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ClassSelection
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
